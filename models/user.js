06610fb3eaef7b8bad2ddad87fe9c17527b1e53d
Rohan Mishra <ronmishra07@gmail.com>
2026-09-07 00:06:34 +0530

    Upload full project files and fix usermodel

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+
+const userSchema = new mongoose.Schema({
+    image: String,
+    email: String,
+    name: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
+
