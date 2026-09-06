8f6a38db1b34920f5f76d26d00eb420fd42bc46f
Rohan Mishra <ronmishra07@gmail.com>
2026-09-07 00:10:22 +0530

    Add edit, update, and delete user functionality

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express =  require('express');
 const app = express();
 const path = require('path');
-const User = require('./models/user');
 const usermodel = require('./models/user');
 
 app.set('view engine', 'ejs');
